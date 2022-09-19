6009a499d9c20911e34feedf2da50e9f0310d2d8
Inam Ul Rehman <87609597+Inam6530@users.noreply.github.com>
2022-09-19 09:43:41 -0400

    fusion chart

--- src/components/ProductsBasic.js
+++ src/components/ProductsBasic.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import FusionChart from './FusionChart'
 
 const ProductsBasic = () => {
   const { products } = useSelector((state) => state)
   return (
     <div>
       <p>Total products: {products.count}</p>
+      <FusionChart />
     </div>
   )
 }
